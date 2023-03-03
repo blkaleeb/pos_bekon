8b4239abef27a1da5fb48fd58e239652e7aec4b0
calebignatius <caleb.ignatius@germs.sg>
2023-03-03 17:59:56 +0700

    add kategori pengeluaran, wallet table

---
+++ database/migrations/2023_03_03_104325_add_kategori_pengeluaran_to_pengeluaran_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddKategoriPengeluaranToPengeluaranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pengeluaran', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_kategori')
+                  ->after('id_pengeluaran');
+            $table->foreign('id_kategori')->references('id')->on('kategori_pengeluarans');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pengeluaran', function (Blueprint $table) {
+            $table->dropColumn('kode_produk');
+            $table->dropForeign('pengeluaran_id_kategori_foreign');
+        });
+    }
+}
