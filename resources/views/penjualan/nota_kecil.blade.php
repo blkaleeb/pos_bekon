565fa7d9775a9fe5ba20291c52309d8f24a41e63
calebignatius <caleb.ignatius@germs.sg>
2023-03-17 09:16:58 +0700

    fix prettier

--- resources/views/penjualan/nota_kecil.blade.php
+++ resources/views/penjualan/nota_kecil.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,44 +8,45 @@
     <title>Nota Kecil</title>
 
     <?php $style = '
-    <style>
-        * {
-            font-family: "consolas", sans-serif;
-        }
-        p {
-            display: block;
-            margin: 3px;
-            font-size: 10pt;
-        }
-        table td {
-            font-size: 9pt;
-        }
-        .text-center {
-            text-align: center;
-        }
-        .text-right {
-            text-align: right;
-        }
-
-        @media print {
-            @page {
-                margin: 0;
-                size: 75mm 
-    '; ?>
-    <?php $style .= !empty($_COOKIE["innerHeight"]) ? $_COOKIE["innerHeight"] . "mm; }" : "}"; ?>
+                                                <style>
+                                                    * {
+                                                        font-family: "consolas", sans-serif;
+                                                    }
+                                                    p {
+                                                        display: block;
+                                                        margin: 3px;
+                                                        font-size: 10pt;
+                                                    }
+                                                    table td {
+                                                        font-size: 9pt;
+                                                    }
+                                                    .text-center {
+                                                        text-align: center;
+                                                    }
+                                                    .text-right {
+                                                        text-align: right;
+                                                    }
+    
+                                                    @media print {
+                                                        @page {
+                                                            margin: 0;
+                                                            size: 75mm
+                                                '; ?>
+    <?php $style .= !empty($_COOKIE['innerHeight']) ? $_COOKIE['innerHeight'] . 'mm; }' : '}'; ?>
     <?php $style .= '
-            html, body {
-                width: 70mm;
-            }
-            .btn-print {
-                display: none;
-            }
-        }
-    </style>
-    '; ?>
+                                                        html, body {
+                                                            width: 70mm;
+                                                        }
+                                                        .btn-print {
+                                                            display: none;
+                                                        }
+                                                    }
+                                                </style>
+                                                '; ?>
 
     {!! $style !!}
 </head>
+
 <body onload="window.print()">
     <button class="btn-print" style="position: absolute; right: 1rem; top: rem;" onclick="window.print()">Print</button>
     <div class="text-center">
@@ -59,7 +61,7 @@
     <div class="clear-both" style="clear: both;"></div>
     <p>No: {{ tambah_nol_didepan($penjualan->id_penjualan, 10) }}</p>
     <p class="text-center">===================================</p>
-    
+
     <br>
     <table width="100%" style="border: 0;">
         @foreach ($detail as $item)
@@ -109,12 +111,13 @@
         let body = document.body;
         let html = document.documentElement;
         let height = Math.max(
-                body.scrollHeight, body.offsetHeight,
-                html.clientHeight, html.scrollHeight, html.offsetHeight
-            );
+            body.scrollHeight, body.offsetHeight,
+            html.clientHeight, html.scrollHeight, html.offsetHeight
+        );
 
         document.cookie = "innerHeight=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        document.cookie = "innerHeight="+ ((height + 50) * 0.264583);
+        document.cookie = "innerHeight=" + ((height + 50) * 0.264583);
     </script>
 </body>
+
 </html>
